2bf0bac7f46bb8ebabb6ab76a4b7460fd01edd96
wahyukurniawan556 <pujiwahyu556@gmail.com>
2024-10-24 01:22:31 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
+Route::get('/profile', [ProfileController::class, 'index']);
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
